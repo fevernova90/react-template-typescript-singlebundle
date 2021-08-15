6c7578dc30bdc5e1062297690213e6a7e7d2322c
Muhaimin Taib <muhaimin@gotelz.com>
2021-08-15 17:04:09 +0800

    Add webpack config with images loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
-export const App= () => {
-  return <h1>React Typescript Webpack Starter Template</h1>
+import "./styles.css"
+import IMAGE from "./react.png"
+import LOGO from "./react.svg"
+
+export const App = () => {
+  return (
+    <>
+      <h1>React Typescript Webpack Starter Template</h1>
+      <img src={IMAGE} alt="React Logo" width="300" height="200" />
+      <img src={LOGO} alt="React Logo" width="300" />
+    </>
+  )
 }
