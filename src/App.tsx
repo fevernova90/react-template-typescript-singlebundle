ff472e8c356c0bb0357c3f82cf166834dc362888
Muhaimin Taib <muhaimin@gotelz.com>
2021-08-16 02:17:28 +0800

    Test husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import "./styles.css"
-import IMAGE from "./react.png"
-import LOGO from "./react.svg"
+import './styles.css'
+import IMAGE from './react.png'
+import LOGO from './react.svg'
+import { ClickCounter } from './ClickCounter'
 
 export const App = () => {
+  const name = 'muhaimin'
   return (
     <>
-      <h1>React Typescript Webpack Starter Template</h1>
+      <h1>
+        React Typescript Webpack Starter Template - {process.env.NODE_ENV} -
+        {process.env.name} - {name}
+      </h1>
       <img src={IMAGE} alt="React Logo" width="300" height="200" />
       <img src={LOGO} alt="React Logo" width="300" />
+      <ClickCounter />
     </>
   )
 }
